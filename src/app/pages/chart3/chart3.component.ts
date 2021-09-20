fdd40b04fe9b6e1485f6488afd30344dd393a81e
Hafsa <74564623+Hafsa38@users.noreply.github.com>
2021-09-20 14:42:57 +0000

    Update chart3.component.ts

--- src/app/pages/chart3/chart3.component.ts
+++ src/app/pages/chart3/chart3.component.ts
@@ -99,7 +99,7 @@ export class Chart3Component implements OnInit {
     const params = new HttpParams()
     .set('year',this.y) 
     .set('month',this.m) 
-    this.http.get(' https://backend-barrages.herokuapp.com/select',{params}) 
+    this.http.get(' https://barrages.herokuapp.com/select',{params}) 
       .subscribe(res => {
       
         let data: any = res['rows'];
